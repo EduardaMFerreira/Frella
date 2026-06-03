a9675e8e16d419c849819461e198b6557314b70b
Eduarda Ferreira <tmariaclara68@gmail.com>
2026-06-02 22:16:38 -0300

    fix: add paths and moduleNameMapper to clientes and prestadores

--- backend/services/clientes-service/jest.config.js
+++ backend/services/clientes-service/jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  moduleFileExtensions: ['ts', 'js'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["ts", "js"],
+  moduleNameMapper: {
+    "^winston$": "<rootDir>/node_modules/winston",
+    "^express$": "<rootDir>/node_modules/express",
+    "^uuid$": "<rootDir>/node_modules/uuid"
+  }
 };
--- backend/services/prestadores-service/jest.config.js
+++ backend/services/prestadores-service/jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/src"],
   moduleFileExtensions: ["ts", "js"],
+  moduleNameMapper: {
+    "^winston$": "<rootDir>/node_modules/winston",
+    "^express$": "<rootDir>/node_modules/express",
+    "^uuid$": "<rootDir>/node_modules/uuid"
+  }
 };
